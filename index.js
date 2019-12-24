027539573389d447b86cfaeec84638991726b0eb
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-12-24 12:55:14 -0500

    port adjust

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 var axios = require("axios");
 var cors = require("cors");
 var bodyParser = require("body-parser");
